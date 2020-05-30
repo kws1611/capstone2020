5cdc5d8e5360d4963b0e90adb346f92b4019f266
rladntjd <kws1611@naver.com>
2020-05-30 16:58:06 +0000

    finalfinal

--- src/gps_node.py
+++ src/gps_node.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
 import serial 
 import pynmea2
 import rospy
@@ -8,29 +7,31 @@ from math import sin, cos, sqrt, pi
 
 class GPS:
     def __init__(self):
-        self.ser = serial.Serial("/dev/serial0",9600,timeout=0.5) 
+        self.ser = serial.Serial("/dev/ttyUSB0",9600,timeout=0.5) 
         self.gps_pub = rospy.Publisher("/gps_data", GpsData ,queue_size=1)
 
         self.gps_data = GpsData()
 
     def process(self):
         self.data = self.ser.readline()             
-         
+        
         if  self.data.find('GGA') > 0:   
             msg = pynmea2.parse(self.data)
 
-            try:
-                self.gps_data.latitude = self.DDM2DD(msg.lat)
-                self.gps_data.longitude = self.DDM2DD(msg.lon)
-                self.gps_data.altitude = msg.altitude
+            if len(msg.lat) == 0:
+                rospy.loginfo_throttle(1, "GPS is not fixed")
+                return
+            
+            #self.gps_data.header.stamp = rospy.Time.now()
+            self.gps_data.latitude = self.DDM2DD(msg.lat)
+            self.gps_data.longitude = self.DDM2DD(msg.lon)
+            self.gps_data.altitude = msg.altitude
 
-                self.gps_pub.publish(self.gps_data)
+            self.gps_pub.publish(self.gps_data)
 
-            except:
-                pass
 
     def DDM2DD(self,x):
-        return float(x) % 100 / 60
+        return int(float(x)/100) + float(x) % 100 / 60
 
 if __name__ == "__main__":
     rospy.init_node("gps_node", anonymous = True)
